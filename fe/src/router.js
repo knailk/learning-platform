f7eff630cacdb37fd9ae6261911e29c704af5d03
knailk <tiendung3719@gmail.com>
2023-07-02 15:32:57 +0700

    fix: fix route

--- fe/src/router.js
+++ fe/src/router.js
@@ -3,20 +3,24 @@ import { Route, Routes } from "react-router-dom";
 import MasterLayout from "./pages/theme/MasterLayout";
 const renderUserRouter = () => {
   return (
-    <Routes>
+    <>
       {CONST_ROUTERS.MENU_NAV_BAR.map((item, index) => {
         if (item.title === "login" || item.title === "register") {
           return (
-            <Route key={index} path={item.path} element={item.component} />
+            <Routes>
+              <Route key={index} path={item.path} element={item.component} />
+            </Routes>
           );
         } else
           return (
             <MasterLayout>
-              <Route key={index} path={item.path} element={item.component} />
+              <Routes>
+                <Route key={index} path={item.path} element={item.component} />
+              </Routes>
             </MasterLayout>
           );
       })}
-    </Routes>
+    </>
   );
 };
 
