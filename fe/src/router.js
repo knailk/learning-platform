c2febab1289adcf8a2e49babfaa87c2ab2cdd712
knailk <tiendung3719@gmail.com>
2023-07-02 15:24:56 +0700

    fix: update router

--- fe/src/router.js
+++ fe/src/router.js
@@ -1,19 +1,26 @@
-import { ROUTERS as CONST_ROUTERS } from './utils/router';
-import { Route, Routes } from 'react-router-dom';
-import MasterLayout from './pages/theme/MasterLayout';
+import { ROUTERS as CONST_ROUTERS } from "./utils/router";
+import { Route, Routes } from "react-router-dom";
+import MasterLayout from "./pages/theme/MasterLayout";
 const renderUserRouter = () => {
-    return (
-        <MasterLayout>
-            <Routes>
-                {CONST_ROUTERS.MENU_NAV_BAR.map((item, index) => {
-                    return <Route key={index} path={item.path} element={item.component} />;
-                })}
-            </Routes>
-        </MasterLayout>
-    );
+  return (
+    <Routes>
+      {CONST_ROUTERS.MENU_NAV_BAR.map((item, index) => {
+        if (item.title === "login" || item.title === "register") {
+          return (
+            <Route key={index} path={item.path} element={item.component} />
+          );
+        } else
+          return (
+            <MasterLayout>
+              <Route key={index} path={item.path} element={item.component} />
+            </MasterLayout>
+          );
+      })}
+    </Routes>
+  );
 };
 
 const RouterCustom = () => {
-    return renderUserRouter();
+  return renderUserRouter();
 };
 export default RouterCustom;
