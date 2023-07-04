2848acdb652041d9b7f766c355a316f953bbbe95
knailk <tiendung3719@gmail.com>
2023-07-04 21:26:31 +0700

    update: update route

--- fe/src/router.js
+++ fe/src/router.js
@@ -1,19 +1,27 @@
-import { ROUTERS as CONST_ROUTERS } from './utils/router';
-import { Route, Routes } from 'react-router-dom';
-import MasterLayout from './pages/theme/MasterLayout';
+import { ROUTERS as CONST_ROUTERS } from "./utils/router";
+import { Route, Routes } from "react-router-dom";
+import MasterLayout from "./pages/theme/MasterLayout";
+import LoginPage from "./pages/user/LoginPage";
 const renderUserRouter = () => {
-    return (
-        <MasterLayout>
-            <Routes>
-                {CONST_ROUTERS.MENU_NAV_BAR.map((item, index) => {
-                    return <Route key={index} path={item.path} element={item.component} />;
-                })}
-            </Routes>
-        </MasterLayout>
-    );
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      {/* <Route exact path="/*" element={<MasterLayout />}> */}
+      {CONST_ROUTERS.MENU_NAV_BAR.map((item, index) => {
+        return (
+          <Route
+            key={index}
+            path={item.path}
+            element={<MasterLayout>{item.component}</MasterLayout>}
+          />
+        );
+      })}
+      {/* </Route> */}
+    </Routes>
+  );
 };
 
 const RouterCustom = () => {
-    return renderUserRouter();
+  return renderUserRouter();
 };
 export default RouterCustom;
