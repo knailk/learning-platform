6a27f9196740d6405248ae3e69eab9d2f5f71d70
knailk <tiendung3719@gmail.com>
2023-07-23 08:25:26 +0700

    update: route api

--- app/api/router.go
+++ app/api/router.go
@@ -2,15 +2,14 @@ package api
 
 import (
 	"context"
-	"errors"
 	"net/http"
 
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
-	"github.com/knailk/learning-platform/app/models"
 	"github.com/knailk/learning-platform/app/controllers"
 	"github.com/knailk/learning-platform/app/middleware"
+	"github.com/knailk/learning-platform/app/models"
 	"github.com/knailk/learning-platform/db"
 	"github.com/knailk/learning-platform/db/postgresql/repository"
 	"github.com/knailk/learning-platform/pkg/log"
@@ -32,7 +31,8 @@ func Handler(ctx context.Context) (*gin.Engine, error) {
 		v1.POST("/user/login", user.Login)
 		v1.POST("/user/register", user.Register)
 		v1.GET("/user/logout", user.Logout)
-		v1.PUT("/user/profile", user.UpdateProfile)
+		v1.PUT("/user/profile", middleware.TokenAuthMiddleware(), user.UpdateProfile)
+		v1.GET("/user/rank", middleware.TokenAuthMiddleware(), user.GetRank)
 
 		/*** START AUTH ***/
 		auth := new(controllers.AuthController)
@@ -40,21 +40,25 @@ func Handler(ctx context.Context) (*gin.Engine, error) {
 		//Refresh the token when needed to generate new access_token and refresh_token for the user
 		v1.POST("/token/refresh", auth.Refresh)
 
-		/*** START Article ***/
-		// article := new(controllers.ArticleController)
+		/*** START Chapter ***/
+		chapter := &controllers.ChapterController{ChapterModel: &models.ChapterModel{Repo: repo}}
 
-		// v1.POST("/article", middleware.TokenAuthMiddleware(), article.Create)
-		// v1.GET("/articles", middleware.TokenAuthMiddleware(), article.All)
-		// v1.GET("/article/:id", middleware.TokenAuthMiddleware(), article.One)
-		// v1.PUT("/article/:id", middleware.TokenAuthMiddleware(), article.Update)
-		// v1.DELETE("/article/:id", middleware.TokenAuthMiddleware(), article.Delete)
+		v1.GET("/chapters", middleware.TokenAuthMiddleware(), chapter.List)
+		v1.GET("/chapters/:id", middleware.TokenAuthMiddleware(), chapter.Get)
+
+		/*** START Chapter ***/
+		lesson := &controllers.LessonController{LessonModel: &models.LessonModel{Repo: repo}}
+
+		v1.GET("/lessons", middleware.TokenAuthMiddleware(), lesson.List)
+		v1.GET("/lessons/:id", middleware.TokenAuthMiddleware(), lesson.Get)
+		v1.GET("/chapters/:id/lessons", middleware.TokenAuthMiddleware(), lesson.ListByChapterID)
 	}
 
 	// route not found
 	r.NoRoute(func(ctx *gin.Context) {
 		log.Error("Route not found", ctx.Request)
 
-		ctx.JSON(http.StatusNotFound, gin.H{"errors": errors.New("Route not found")})
+		ctx.JSON(http.StatusNotFound, gin.H{"errors": "Route not found"})
 	})
 
 	return r, nil
--- app/entity/question.go
+++ app/entity/question.go
@@ -10,7 +10,7 @@ import (
 type Question struct {
 	ID            uuid.UUID      `json:"id" gorm:"type:uuid;default:gen_random_uuid()"`
 	LessonID      uuid.UUID      `json:"lesson_id"`
-	Order         int            `json:"order"`
+	Level         int            `json:"level"`
 	Score         pq.Int64Array  `json:"score" gorm:"type:integer[]"`
 	AnswerType    string         `json:"answer_type"`
 	AnswerContent pq.StringArray `json:"answer_content" gorm:"type:text[]"`
