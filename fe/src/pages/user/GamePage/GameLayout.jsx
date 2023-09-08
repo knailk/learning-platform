3b75d8bf5db1789ec7d8cf621db239f299db744a
tranquyetthang281 <79098631+tranquyetthang281@users.noreply.github.com>
2023-09-08 23:47:10 +0700

    input game

--- fe/src/pages/user/GamePage/GameLayout.jsx
+++ fe/src/pages/user/GamePage/GameLayout.jsx
@@ -1,14 +1,31 @@
-import React,{ memo } from 'react';
+import React, { memo, Fragment } from 'react';
 import styles from './style.module.scss';
 import { Row, Col } from 'antd';
 import BoardControl from './BoardControl';
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 const GameLayout = () => {
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "Build/Output.loader.js",
+        dataUrl: "Build/Output.data",
+        frameworkUrl: "Build/Output.framework.js",
+        codeUrl: "Build/Output.wasm",
+    });
+
     return (
         <>
             <Row>
                 <Col>
-                    <Row className={styles.gameLayout}></Row>
+                    <Row className={styles.gameLayout}>
+                        <Fragment>
+                            <Unity style={{
+                                width: "1024px",
+                                height: "576px",
+                                justifySelf: "center",
+                                alignSelf: "center",
+                            }} unityProvider={unityProvider} />
+                            </Fragment>
+                    </Row>
                     <Col></Col>
                 </Col>
                 <BoardControl />
