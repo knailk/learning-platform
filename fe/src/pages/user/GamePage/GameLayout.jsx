cccc0b6d2ffe28d384a55ddc1d0b858a16ba8649
tranminhtoan149 <tranminhtoan.149@gmail.com>
2023-09-09 00:30:56 +0700

    update

--- fe/src/pages/user/GamePage/GameLayout.jsx
+++ fe/src/pages/user/GamePage/GameLayout.jsx
@@ -1,15 +1,16 @@
 import React, { memo, Fragment } from 'react';
 import styles from './style.module.scss';
 import { Row, Col } from 'antd';
-import BoardControl from './BoardControl';
-import { Unity, useUnityContext } from "react-unity-webgl";
+// import BoardControl from './BoardControl';
+import CodeControl from './CodeControl';
+import { Unity, useUnityContext } from 'react-unity-webgl';
 
 const GameLayout = () => {
     const { unityProvider } = useUnityContext({
-        loaderUrl: "Build/Output.loader.js",
-        dataUrl: "Build/Output.data",
-        frameworkUrl: "Build/Output.framework.js",
-        codeUrl: "Build/Output.wasm",
+        loaderUrl: 'Build/Output.loader.js',
+        dataUrl: 'Build/Output.data',
+        frameworkUrl: 'Build/Output.framework.js',
+        codeUrl: 'Build/Output.wasm',
     });
 
     return (
@@ -18,17 +19,20 @@ const GameLayout = () => {
                 <Col>
                     <Row className={styles.gameLayout}>
                         <Fragment>
-                            <Unity style={{
-                                width: "1024px",
-                                height: "576px",
-                                justifySelf: "center",
-                                alignSelf: "center",
-                            }} unityProvider={unityProvider} />
-                            </Fragment>
+                            <Unity
+                                style={{
+                                    width: '1024px',
+                                    height: '576px',
+                                    justifySelf: 'center',
+                                    alignSelf: 'center',
+                                }}
+                                unityProvider={unityProvider}
+                            />
+                        </Fragment>
                     </Row>
                     <Col></Col>
                 </Col>
-                <BoardControl />
+                <CodeControl />
             </Row>
         </>
     );
