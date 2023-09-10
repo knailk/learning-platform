9d65733a6bfbc9613f2cd78a4ce927359a1f790a
tranminhtoan149 <tranminhtoan.149@gmail.com>
2023-09-10 23:34:38 +0700

    update

--- node_server/index.js
+++ node_server/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const cors = require('cors');
 const express = require('express');
 const PythonShell = require('python-shell').PythonShell;
-const defaultPath = '../fe/public/code';
+const defaultPath = './code';
 const PORT = 80;
 const app = express();
 
@@ -10,14 +10,48 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/python', (req, res) => {
+    let file_name = Date.now() + '.py';
     try {
         let date = new Date();
         let file_path = defaultPath + '/' + req.body.user_id + '/code_test/';
         file_path += date.getFullYear() + '_';
         file_path += date.getMonth() + '_';
         file_path += date.getDate();
-        let file_name = Date.now() + '.py';
 
+        //check if folder exist
+        if (!fs.existsSync(file_path)) {
+            fs.mkdirSync(file_path, { recursive: true });
+        }
+        //add file
+        fs.appendFile(file_name, '', function (err) {
+            if (err) throw err;
+        });
+        //write file
+        fs.writeFile(file_path + '/' + file_name, req.body.code);
+        let options = {
+            mode: 'text',
+            pythonOptions: ['-u'],
+            args: [1, 2, 3],
+        };
+        PythonShell.run(file_path + '/' + file_name, options)
+            .then((messages) => {
+                res.send({ data: messages.join('\r\n') });
+                fs.unlinkSync(file_name);
+            })
+            .catch((err) => {
+                res.send({ data: err.traceback });
+                fs.unlinkSync(file_name);
+            });
+    } catch (error) {
+        res.send({ data: 'Lỗi hệ thống, vui lòng thử lại sau' });
+        fs.unlinkSync(file_name);
+    }
+});
+
+app.post('/code-practice', (req, res) => {
+    let file_name = req.body.practice_code_id + '.py';
+    try {
+        let file_path = defaultPath + '/' + req.body.user_id + '/code_practice';
         //check if folder exist
         if (!fs.existsSync(file_path)) {
             fs.mkdirSync(file_path, { recursive: true });
@@ -40,41 +74,11 @@ app.post('/python', (req, res) => {
             })
             .catch((err) => {
                 res.send({ data: err.traceback });
+                fs.unlinkSync(file_name);
             });
     } catch (error) {
         res.send({ data: 'Lỗi hệ thống, vui lòng thử lại sau' });
-    }
-});
-
-app.post('/code-practice', (req, res) => {
-    try {
-        let file_path = defaultPath + '/' + req.body.user_id + '/code_practice';
-        let file_name = req.body.practice_code_id + '.py';
-        //check if folder exist
-        // if (!fs.existsSync(file_path)) {
-        //     fs.mkdirSync(file_path, { recursive: true });
-        // }
-        //add file
-        // fs.appendFile(file_name, '', function (err) {
-        //     if (err) throw err;
-        // });
-        // //write file
-        fs.writeFileSync(file_path + '/' + file_name, req.body.code);
-        let options = {
-            mode: 'text',
-            pythonOptions: ['-u'],
-            args: [1, 2, 3],
-        };
-        PythonShell.run(file_path + '/' + file_name, options)
-            .then((messages) => {
-                res.send({ data: messages.join('\r\n') });
-                // fs.unlinkSync(file_name);
-            })
-            .catch((err) => {
-                res.send({ data: err.traceback });
-            });
-    } catch (error) {
-        res.send({ data: 'Lỗi hệ thống, vui lòng thử lại sau' });
+        fs.unlinkSync(file_name);
     }
 });
 
