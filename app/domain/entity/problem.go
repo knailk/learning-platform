61cf54755e3e1a5ac18854d944027a5bef4a0d04
knailk <tiendung3719@gmail.com>
2023-09-24 03:05:50 +0700

    chore: implement value and scan

--- app/domain/entity/problem.go
+++ app/domain/entity/problem.go
@@ -1,6 +1,9 @@
 package entity
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -14,7 +17,7 @@ type Problem struct {
 	Level         ProblemLevel `json:"level"`
 	URL           string       `json:"url" gorm:"unique"`
 	FunctionName  string       `json:"function_name"`
-	Args          []Arg        `json:"args" gorm:"type:jsonb"`
+	Args          Args         `json:"args" gorm:"type:jsonb"`
 	AvailableCode string       `json:"available_code"`
 	SolutionCode  string       `json:"solution_code"`
 	UpdatedAt     time.Time    `json:"-" gorm:"default:CURRENT_TIMESTAMP()"`
@@ -28,6 +31,24 @@ type Arg struct {
 	Type  string `json:"type"`
 	Value string `json:"value"`
 }
+
+type Args []Arg
+
+func (a Args) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
+
+// Make the Attrs struct implement the sql.Scanner interface. This method
+// simply decodes a JSON-encoded value into the struct fields.
+func (a *Args) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(b, &a)
+}
+
 
 type ProblemLevel string
 
