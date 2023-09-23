73675fd4bf959c3355e497c3c4419b350de9d08a
knailk <tiendung3719@gmail.com>
2023-09-24 03:45:13 +0700

    chore: change type

--- app/domain/entity/test_case.go
+++ app/domain/entity/test_case.go
@@ -9,7 +9,7 @@ import (
 type TestCase struct {
 	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid()"`
 	ProblemID uuid.UUID `json:"problem_id"`
-	Input     []Arg     `json:"input" gorm:"type:jsonb"`
+	Input     Args     `json:"input" gorm:"type:jsonb"`
 
 	UpdatedAt time.Time `json:"-" gorm:"default:CURRENT_TIMESTAMP()"`
 	CreatedAt time.Time `json:"created_at" gorm:"default:CURRENT_TIMESTAMP()"`
